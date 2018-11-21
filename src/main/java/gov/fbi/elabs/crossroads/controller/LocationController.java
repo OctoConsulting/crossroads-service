af2a5bbf2126fdc7cf4224ce29989f451b768aae
Nithin Emanuel <nithinemanuel@Nithin-Emanuel-MacBook-Pro.local>
2018-11-20 22:23:59 -0500

    test

--- src/main/java/gov/fbi/elabs/crossroads/controller/LocationController.java
+++ src/main/java/gov/fbi/elabs/crossroads/controller/LocationController.java
@@ -1,20 +1,14 @@
 package gov.fbi.elabs.crossroads.controller;
 
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import gov.fbi.elabs.crossroads.domain.Location;
 import gov.fbi.elabs.crossroads.exception.BaseApplicationException;
-import gov.fbi.elabs.crossroads.service.LocationService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
@@ -25,14 +19,14 @@ public class LocationController {
 
 	private static final Logger logger = LoggerFactory.getLogger(LocationController.class);
 	
-	@Autowired
-	private LocationService locationService;
-	
-	@RequestMapping(method = RequestMethod.GET)
-    @ApiOperation(value = "Fetch all workflow")
-	public List<Location> getAllLocations() throws BaseApplicationException{
-		return locationService.getAllLocations();
-	}
+//	@Autowired
+//	private LocationService locationService;
+//	
+//	@RequestMapping(method = RequestMethod.GET)
+//    @ApiOperation(value = "Fetch all workflow")
+//	public List<Location> getAllLocations() throws BaseApplicationException{
+//		return locationService.getAllLocations();
+//	}
 	
 	@RequestMapping(value = "/test", method = RequestMethod.GET)
     @ApiOperation(value = "Fetch all workflow")

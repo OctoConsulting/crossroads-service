fd89282e0e042b4e025e89cc500eddf1078609e1
vinodcherukuri <vinod.cherukuri@gmail.com>
2018-12-11 16:32:31 -0500

    Login and logout changes

---
+++ src/main/java/gov/fbi/elabs/crossroads/session/Initializer.java
@@ -0,0 +1,7 @@
+package gov.fbi.elabs.crossroads.session;
+
+import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
+
+public class Initializer extends AbstractHttpSessionApplicationInitializer {
+
+}

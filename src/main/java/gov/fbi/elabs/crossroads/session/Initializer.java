77f90d2861966de68f9435c231fff12fe5991f2b
Nithin Emanuel <nithinemanuel@Nithin-Emanuel-MacBook-Pro.local>
2018-12-17 15:31:42 -0500

    filters

--- src/main/java/gov/fbi/elabs/crossroads/session/Initializer.java
+++ src/main/java/gov/fbi/elabs/crossroads/session/Initializer.java
@@ -1,7 +1,5 @@
 package gov.fbi.elabs.crossroads.session;
 
-import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
-
-public class Initializer extends AbstractHttpSessionApplicationInitializer {
+public class Initializer {// extends AbstractHttpSessionApplicationInitializer {
 
 }
--- src/main/java/gov/fbi/elabs/crossroads/session/SecurityInitializer.java
+++ src/main/java/gov/fbi/elabs/crossroads/session/SecurityInitializer.java
@@ -1,14 +1,10 @@
 package gov.fbi.elabs.crossroads.session;
 
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-import gov.fbi.elabs.crossroads.ldap.SecurityConfig;
-
-
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+public class SecurityInitializer { // extends
+									// AbstractSecurityWebApplicationInitializer
+									// {
 
 	public SecurityInitializer() {
-		super(SecurityConfig.class, SessionConfig.class);
+		// super(SecurityConfig.class, SessionConfig.class);
 	}
 }

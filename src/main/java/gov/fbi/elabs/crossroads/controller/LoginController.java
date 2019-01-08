d60eabba2ec19218fcfeef1c8ba962d553a6e001
vinodcherukuri <vinod.cherukuri@gmail.com>
2019-01-08 15:48:38 -0500

    Login Logout changes

--- src/main/java/gov/fbi/elabs/crossroads/controller/LoginController.java
+++ src/main/java/gov/fbi/elabs/crossroads/controller/LoginController.java
@@ -12,7 +12,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.session.data.redis.RedisOperationsSessionRepository;
-import org.springframework.session.data.redis.RedisOperationsSessionRepository.RedisSession;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;

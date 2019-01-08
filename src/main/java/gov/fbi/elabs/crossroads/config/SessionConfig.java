38837f589a8bd8a033e52a06422e0a4ea225e145
vinodcherukuri <vinod.cherukuri@gmail.com>
2019-01-08 12:17:09 -0500

    Login issues

--- src/main/java/gov/fbi/elabs/crossroads/config/SessionConfig.java
+++ src/main/java/gov/fbi/elabs/crossroads/config/SessionConfig.java
@@ -1,8 +1,6 @@
 package gov.fbi.elabs.crossroads.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
 
 @Configuration
 //@EnableRedisHttpSession
